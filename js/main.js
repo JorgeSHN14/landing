53b59ff55132c7d4b34f9b63f75d3f6c38861f7b
bmitegui <bmite@hubilogist.com>
2024-10-23 10:52:44 -0500

    Animation

--- js/main.js
+++ js/main.js
@@ -9,9 +9,22 @@ let loaded = () => {
         eventSubmit.preventDefault();
         let emailElement = document.querySelector('.form-control-lg');
         let emailText = emailElement.value;
-        if(emailText.length === 0){
+        if (emailText.length === 0) {
             emailElement.focus();
         }
+        const keyframes = [
+            { transform: 'translateX(0)' },
+            { transform: 'translateX(50px)' },
+            { transform: 'translateY(-50px)' },
+            { transform: 'translateY(0px)' },
+        ];
+        const timing = {
+            duration: 400, // in milliseconds
+            iterations: 1, // repeat indefinitely
+        };
+        const animation = new KeyframeEffect(emailElement, keyframes, timing);
+        const animationPlayer = new Animation(animation);
+        animationPlayer.play();
     })
 }
 window.addEventListener("DOMContentLoaded", ready);
